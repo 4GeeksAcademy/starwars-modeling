f8b18661fa7d76e2f58426f3b788977c8c780dd1
NathanMB77 <160797692+NathanMB77@users.noreply.github.com>
2024-05-21 19:52:08 +0000

    done

--- src/models.py
+++ src/models.py
@@ -7,26 +7,72 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(20), nullable=False)
+    password = Column(String(30), nullable=False)
+    email = Column(String, nullable=True)
+    def serialize(self):
+        return {
+            'email': self.email,
+            'username': self.username
+            }
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(30), nullable=False)
+    population = Column(Integer, nullable=False)
+    def serialize(self):
+        return {
+            'name': self.name,
+            'population': self.population
+            }
+    
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+    age = Column(Integer, nullable=False)
+    def serialize(self):
+        return {
+            'name': self.name,
+            'age': self.age
+            }
 
-    def to_dict(self):
-        return {}
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+    top_speed = Column(Integer, nullable=False)
+    def serialize(self):
+        return {
+            'name': self.name,
+            'top_speed': self.top_speed
+            }
+
+class FavoriteList(Base):
+    __tablename__ = 'favoriteList'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    user = relationship(User)
+    planet = relationship(Planet)
+    character = relationship(Character)
+    vehicle = relationship(Vehicle)
+    def serialize(self):
+        return {
+            'planet': self.planet,
+            'user': self.user,
+            'vehicle': self.vehicle,
+            'character': self.character
+            }
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
